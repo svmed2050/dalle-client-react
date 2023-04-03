12e98796cae7831aa369242b31f1c05a72d82d9c
Pavel <pavelsergienko7@gmail.com>
2023-04-03 19:53:52 +0300

    change link for server

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,18 +17,23 @@ const Home = () => {
 	const [allPosts, setAllPosts] = useState(null)
 
 	const [searchText, setSearchText] = useState('')
+	const [searchedResults, setSearchedResults] = useState(null)
+	const [searchTimeout, setSearchTimeout] = useState(null)
 
 	useEffect(() => {
 		const fetchPosts = async () => {
 			setLoading(true)
 
 			try {
-				const response = await fetch('http://localhost:8080/api/v1/post', {
-					method: 'GET',
-					headers: {
-						'Content-Type': 'application/json',
-					},
-				})
+				const response = await fetch(
+					'https://grumpy-pink-spacesuit.cyclic.app/api/v1/post',
+					{
+						method: 'GET',
+						headers: {
+							'Content-Type': 'application/json',
+						},
+					}
+				)
 
 				if (response.ok) {
 					const result = await response.json()
@@ -45,6 +50,23 @@ const Home = () => {
 		fetchPosts()
 	}, [])
 
+	const handleSearchChange = (e) => {
+		// setSearchedResults(allPosts)
+		clearTimeout(searchTimeout)
+		setSearchText(e.target.value)
+
+		setSearchTimeout(
+			setTimeout(() => {
+				const searchResults = allPosts.filter(
+					(item) =>
+						item.name.toLowerCase().includes(searchText.toLowerCase()) ||
+						item.prompt.toLowerCase().includes(searchText.toLowerCase())
+				)
+				setSearchedResults(searchResults)
+			}, 500)
+		)
+	}
+
 	return (
 		<section className='max-w-7xl mx-auto'>
 			<div>
@@ -58,7 +80,14 @@ const Home = () => {
 			</div>
 
 			<div className='mt-16'>
-				<FormField />
+				<FormField
+					labelName='Search posts'
+					type='text'
+					name='text'
+					placeholder='Search posts'
+					value={searchText}
+					handleChange={handleSearchChange}
+				/>
 			</div>
 
 			<div className='mt-10'>
@@ -76,7 +105,10 @@ const Home = () => {
 						)}
 						<div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
 							{searchText ? (
-								<RenderCards data={[]} title='No search results found' />
+								<RenderCards
+									data={searchedResults}
+									title='No search results found'
+								/>
 							) : (
 								<RenderCards data={allPosts} title='No posts found' />
 							)}
