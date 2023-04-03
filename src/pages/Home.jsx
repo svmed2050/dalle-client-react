c5cc050a5f3081fc896248219c0bb64f4b25f5ff
Pavel <pavelsergienko7@gmail.com>
2023-04-03 13:12:29 +0300

    added RenderCards component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,16 @@ import React, { useState, useEffect } from 'react'
 
 import { Card, FormField, Loader } from '../components'
 
+const RenderCards = ({ data, title }) => {
+	if (data?.length > 0) {
+		return data.map((post) => <Card key={post._id} {...post} />)
+	}
+
+	return (
+		<h2 className='mt-5 font-bold text-[#6449ff] text-xl uppercase'>{title}</h2>
+	)
+}
+
 const Home = () => {
 	const [loading, setLoading] = useState(false)
 	const [allPosts, setAllPosts] = useState(null)
@@ -37,6 +47,16 @@ const Home = () => {
 								<span className='text-[#222328]'>{searchText}</span>
 							</h2>
 						)}
+						<div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
+							{searchText ? (
+								<RenderCards
+									data='searchResults'
+									title='No search results found'
+								/>
+							) : (
+								<RenderCards data='allPosts' title='No posts found' />
+							)}
+						</div>
 					</>
 				)}
 			</div>
