154999ffa094a26c5359cbbcabb4fe19f5b72a0f
Pavel <pavelsergienko7@gmail.com>
2023-04-03 14:38:52 +0300

    added createPost && formField components

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -16,6 +16,12 @@ const CreatePost = () => {
 	const [generatingImg, setGeneratingImg] = useState(false)
 	const [loading, setLoading] = useState(false)
 
+	const handleSubmit = () => {}
+
+	const handleChange = (e) => {}
+
+	const handleSupriseMe = () => {}
+
 	return (
 		<section className='max-w-7xl mx-auto'>
 			<div>
@@ -25,6 +31,51 @@ const CreatePost = () => {
 					share them with the community
 				</p>
 			</div>
+
+			<form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+				<div className='flex flex-col gap-5'>
+					<FormField
+						labelName='Your name'
+						type='text'
+						name='name'
+						placeholder='John Doe'
+						value={form.name}
+						handleChange={handleChange}
+					/>
+					<FormField
+						labelName='Prompt'
+						type='text'
+						name='prompt'
+						placeholder='A plush toy robot sitting against a yellow wall'
+						value={form.prompt}
+						handleChange={handleChange}
+						isSurpriseMe
+						handleSupriseMe={handleSupriseMe}
+					/>
+
+					<div className='relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue=500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center'>
+						{form.photo ? (
+							<img
+								src={form.photo}
+								alt={form.photo}
+								clasName='w-full h-full object-contain'
+							/>
+						) : (
+							<img
+								src={preview}
+								alt='preview'
+								className='w-9/12 h-9/12 object-contain opacity-40'
+							/>
+						)}
+
+						{generatingImg && (
+							<div className='absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg'>
+								<Loader />
+							</div>
+						)}
+					</div>
+				</div>
+			</form>
 		</section>
 	)
 }
