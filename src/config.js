2183ad50a8a54a4b74070b65f8106aea071afd42
Pavel <pavelsergienko7@gmail.com>
2023-04-04 10:24:49 +0300

    change serverUrl in config

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const serverUrl = 'https://grumpy-pink-spacesuit.cyclic.app'
-export const serverUrl = 'http://localhost:8080'
+export const serverUrl = 'https://grumpy-pink-spacesuit.cyclic.app'
+// export const serverUrl = 'http://localhost:8080'
